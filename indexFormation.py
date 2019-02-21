57ed0a0846b791647d97e50a64c62aea26b768ec
Siddhant Sonkar <siddhant.sonkar@gmail.com>
2019-02-21 01:11:33 -0800

    Added write to JSON file

--- indexFormation.py
+++ indexFormation.py
@@ -1,13 +1,14 @@
-import zipfile
+import json
 
+
+def merge_maps(old_freq, doc_freq):
+    #TODO add doc_freq to old_freq lexicographically
+    return old_freq
 
 def addTokens(token_freq, file_no):
-    zfile_name = ''
-    with zipfile.ZipFile(zfile_name) as z:
-        for f_name in z.namelist():
-            for token in token_freq:
-                f_name = str(token[0])+''
-                f = open(f_name, a)
-                txt = token+":"+token_freq[token]+':'+file_no+'\\n'
-                f.write(txt)
-                f.close()
+    for c in token_freq.keys():
+        file_name = "data/" + c + ".json" 
+        old_freq = json.loads(open(file_name).read())      
+        new_freq = merge_maps(old_freq, token_freq[c])
+        with open(file_name, 'w') as file_new:
+            json.dump(new_freq, file_new)
