2e7b7d4adff8b6f6605f4f7448ea742f2b23cecb
Karan <karanrgowda75.com>
2019-02-20 23:15:42 -0800

    indexFormation has been added

--- tokenizer.py
+++ tokenizer.py
@@ -1,28 +1,20 @@
 from bs4 import BeautifulSoup
 from bs4.element import Comment
-import urllib
+import urllib.request
 import collections
 
 
 def generateTokens(input_string):
-    temp_token = input_string.split(' ')
-    parse = []
-    for temp in temp_token:
-        parse.append(temp.encode('ascii', 'ignore'))
-
     Token = []
-    for word in parse:
-        temp = ''
-        for c in word:
-            if(c.isdigit() or c.isalpha()):
-                temp += c.lower()
-            elif(temp != ''):
-                Token.append(temp)
-                temp = ''
-        Token.append(temp)
+    temp = ''
+    for c in input_string:
+        if(c.isdigit() or c.isalpha()):
+            temp += c.lower()
+        elif(temp != ''):
+            Token.append(temp)
+            temp = ''
     if(temp != ''):
         Token.append(temp)
-        temp = ''
     return Token
 
 
@@ -34,16 +26,36 @@ def tag_visible(element):
     return True
 
 
+def formTokenLists(raw_list):
+    raw_list_temp = []
+    for i in raw_list:
+        raw_list_temp.append(i.get_text())
+    out = []
+    for k in raw_list_temp:
+        out.extend(generateTokens(k))
+    out = list(set(out))
+    return out
+
+def heuristics():
+    pass
+
+
 def text_from_html(body):
     soup = BeautifulSoup(body, 'html.parser')
     texts = soup.findAll(text=True)
     visible_texts = filter(tag_visible, texts)
+
+    title = formTokenLists(soup.find_all('title'))
+    headers_1_3 = formTokenLists(soup.find_all(['h1', 'h2', 'h3']))
+    headers_4_6 = formTokenLists(soup.find_all(['h4', 'h5', 'h6']))
+    body = formTokenLists(soup.find_all('body'))
     return u" ".join(t.strip() for t in visible_texts)
 
 
 def parserMain(path):
-    html = urllib.request.urlopen(path).read()
-    page_string = text_from_html(html)
+    # html = urllib.request.urlopen(path).read()
+    f = open(path)
+    page_string = text_from_html(f)
 
     Token = generateTokens(page_string)
 
@@ -51,4 +63,7 @@ def parserMain(path):
 
     if('' in token_frequency):
         del token_frequency['']
-    return token_frequency
+    
+
+
+parserMain('0')
