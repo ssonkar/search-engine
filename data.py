785659b9258d760430b131ad2adb15d9538a950f
Siddhant Sonkar <siddhant.sonkar@gmail.com>
2019-02-20 20:49:03 -0800

    Code fixes

--- data.py
+++ data.py
@@ -1,14 +1,15 @@
 def read_texts(zipfname):
     import zipfile
     import os
+    import tokenizer
     with zipfile.ZipFile(zipfname) as z:
-        for foldername in z.namelist():
-            if not os.path.isdir(foldername):
-                print('Reading File '+ foldername)
+        for filename in z.namelist():
+            if not os.path.isdir(filename):
+                print('Reading File '+ filename)
+                with z.open(filename) as f:
+                    freq = tokenizer.parserMain(f)
 
         
 
 if __name__ == "__main__":
     read_texts("data/webpages.zip")
-    
-
