f9073f12c57de107e9584731abf652ee3e313dc2
Siddhant Sonkar <siddhant.sonkar@gmail.com>
2019-02-20 19:51:09 -0800

    Zip read

--- data.py
+++ data.py
@@ -1,8 +1,14 @@
-def read_text(zipfname):
+def read_texts(zipfname):
     import zipfile
     import os
     with zipfile.ZipFile(zipfname) as z:
         for foldername in z.namelist():
             if not os.path.isdir(foldername):
-                parser
+                print('Reading File '+ foldername)
+
+        
+
+if __name__ == "__main__":
+    read_texts("data/webpages.zip")
+    
 
