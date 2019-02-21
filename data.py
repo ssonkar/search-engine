10eb99891eccd5d724b5f9158966ae34ccc84db0
Siddhant Sonkar <siddhant.sonkar@gmail.com>
2019-02-21 00:57:02 -0800

    Limiting files

--- data.py
+++ data.py
@@ -7,11 +7,14 @@ def read_texts(zipfname):
     import tokenizer
     import json
     import pprint
+    files = 0
     with zipfile.ZipFile(zipfname) as z:
         for filename in z.namelist():
             if not filename[-1] == '/' and '.tsv' not in filename and '.json' not in filename:
-                print('Reading File ' + filename)
-                tokenizer.parserMain(z.read(filename), filename[filename.index("/")+1:])
+                if files <3 :
+                    print('Reading File ' + filename)
+                    tokenizer.parserMain(z.read(filename), filename[filename.index("/")+1:])
+                    files +=1 
             elif '.json' in filename:
                 json_data = z.read(filename)
                 globals.code2url = json.loads(json_data.decode("utf-8"))
