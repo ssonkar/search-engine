b0d9b4e48f8a5b4815893d89f0b4456c7365e363
Siddhant Sonkar <siddhant.sonkar@gmail.com>
2019-02-20 22:38:18 -0800

    Functionality for file path 2 url

--- data.py
+++ data.py
@@ -1,15 +1,22 @@
+import globals  
+
 def read_texts(zipfname):
     import zipfile
     import os
     import tokenizer
+    import json
+    import pprint
     with zipfile.ZipFile(zipfname) as z:
         for filename in z.namelist():
-            if not filename[-1]=='/' :
+            if not filename[-1]=='/' and '.tsv' not in filename and '.json' not in filename:
                 print('Reading File '+ filename)
-                freq = tokenizer.parserMain(z.read(filename))
-                print(freq)
-
-        
+                #freq = tokenizer.parserMain(z.read(filename))
+            elif '.json' in filename:
+                json_data = z.read(filename)
+                globals.code2url = json.loads(json_data.decode("utf-8"))
+                print(globals.code2url)
+      
 
 if __name__ == "__main__":
+    globals.init
     read_texts("data/webpages.zip")
---
+++ globals.py
@@ -0,0 +1,3 @@
+def init():
+    global code2url
+    code2url = dict()
