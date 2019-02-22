349e91781d7139937552b6351d92eff3f94a8f2c
Karan <karanrgowda75.com>
2019-02-21 23:15:45 -0800

    search fixed

--- searchForTokens.py
+++ searchForTokens.py
@@ -7,13 +7,14 @@ def process_query(query):
     stemmed_query = tokenizer.generateTokens(query)
     file_path = "dump/"+str(stemmed_query[0][0])+'.json'
     result_json = settings.read_json(file_path)
-    if(result_json.get(stemmed_query[0][0])):
-        for doc_id, freq in result_json[stemmed_query[0][0]]:
-            result_urls.append(settings.code2url[doc_id])
+    for stem in stemmed_query:
+        if(result_json.get(stem)):
+            if(result_json[stem] != []):
+                for doc_id in result_json[stem]:
+                    result_urls.append(settings.code2url[doc_id[0]])
     return result_urls
 
 if __name__ == "__main__":
-    res = process_query('by')
     res1 = process_query('Informatics')
     res2 = process_query('Mondego')
     res3 = process_query('Irvine')
