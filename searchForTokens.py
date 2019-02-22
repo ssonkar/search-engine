409dc0a5cfeef06d8f178a8a0c5f019748072745
Siddhant Sonkar <siddhant.sonkar@gmail.com>
2019-02-21 22:53:19 -0800

    Code fixes

--- searchForTokens.py
+++ searchForTokens.py
@@ -5,14 +5,15 @@ import settings
 def process_query(query):
     result_urls = []
     stemmed_query = tokenizer.generateTokens(query)
-    file_path = "data/"+str(stemmed_query[0][0])+'.json'
+    file_path = "dump/"+str(stemmed_query[0][0])+'.json'
     result_json = settings.read_json(file_path)
-    if(result_json.get(stemmed_query)):
-        for doc_id, freq in result_json[stemmed_query]:
+    if(result_json.get(stemmed_query[0][0])):
+        for doc_id, freq in result_json[stemmed_query[0][0]]:
             result_urls.append(settings.code2url[doc_id])
     return result_urls
 
 if __name__ == "__main__":
+    res = process_query('by')
     res1 = process_query('Informatics')
     res2 = process_query('Mondego')
     res3 = process_query('Irvine')
