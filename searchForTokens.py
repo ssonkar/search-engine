b49dac805faf5507ad112b0e263b121faa403de6
Siddhant Sonkar <siddhant.sonkar@gmail.com>
2019-02-21 23:31:58 -0800

    Working Code

--- searchForTokens.py
+++ searchForTokens.py
@@ -6,18 +6,17 @@ def process_query(query):
     result_urls = []
     stemmed_query = tokenizer.generateTokens(query)
     file_path = "dump/"+str(stemmed_query[0][0])+'.json'
-    result_json = settings.read_json(file_path)
+    char_2_doc_file = settings.read_json(file_path)
     for stem in stemmed_query:
-        if(result_json.get(stem)):
-            if(result_json[stem] != []):
-                for doc_id in result_json[stem]:
-                    temp = doc_id[0]
-                    result_urls.append(settings.code2url[temp[0]])
+        if(char_2_doc_file.get(stem)):
+            for doc_id in char_2_doc_file[stem][0]:
+                result_urls.append(settings.code2url[doc_id[0]])
     return result_urls
 
 if __name__ == "__main__":
     settings.code2url = settings.read_json("dump/bookkeeping.json")
     res = process_query('anglia')
+    print(res)
     res1 = process_query('Informatics')
     res2 = process_query('Mondego')
     res3 = process_query('Irvine')
